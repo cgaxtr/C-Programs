42ea98284ef8fe3f52a3ec0a48074f8f705cf0df
Carlos <cga.xtr@gmail.com>
2017-12-07 12:54:48 +0100

    added scheduler first-come, first-served

--- schedsim/sched_fcfs.c
+++ schedsim/sched_fcfs.c
@@ -2,28 +2,33 @@
 
 static task_t* pick_next_task_fcfs( runqueue_t* rq )
 {
-	task_t* t = head_slist(rq);
+	task_t* t = head_slist(&rq->tasks);
 
 	if (t)
-		remove_slist(rq->tasks, t);
+		remove_slist(&rq->tasks,t);
 
 	return t;
 }
 
 static void enqueue_task_fcfs(task_t* t,runqueue_t* rq, int preempted)
 {
+	if (t->on_rq || is_idle_task(t))
+			return;
 
+	insert_slist(&rq->tasks,t);
 }
+
 
 static task_t* steal_task_fcfs(runqueue_t* rq)
 {
-	task_t* t = tail_slist(rq);
+	task_t* t = tail_slist(&rq->tasks);
 
 	if (t)
-		remove_slist(rq->tasks, t);
+		remove_slist(&rq->tasks, t);
 
 	return t;
 }
+
 
 sched_class_t fcfs_sched={
 		.pick_next_task=pick_next_task_fcfs,
